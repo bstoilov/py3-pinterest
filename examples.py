9cbac98c7232d9fabd949f090c70cf3f6df0cb38
bstoilov <boriostoilov@gmail.com>
2019-08-14 19:21:07 +0300

    initial repo setup

---
+++ examples.py
@@ -0,0 +1,183 @@
+from py3pin.Pinterest import Pinterest
+
+pinterest = Pinterest(email='your email goes here', password='password goes here', username='look in pinterest url')
+
+
+def login():
+    # don't spam this, or they will lock the account
+    return pinterest.login()
+
+
+def get_user_profile():
+    return pinterest.get_user_overview()
+
+
+def get_user_boards():
+    # load specific user boards
+    # board_batch = pinterest.boards(username='username')
+
+    # load current user boards
+    boards = []
+    board_batch = pinterest.boards()
+    while len(board_batch) > 0:
+        boards += board_batch
+        board_batch = pinterest.boards()
+
+    return boards
+
+
+def get_board_pins():
+    board_id = 'board_id'
+    board_url = 'board_url'
+    board_feed = []
+    feed_batch = pinterest.board_feed(board_id=board_id, board_url=board_url)
+    while len(feed_batch) > 0:
+        board_feed += feed_batch
+        feed_batch = pinterest.board_feed(board_id=board_id, board_url=board_url)
+
+    return board_feed
+
+
+def delete_pin():
+    # if pin doesn't exist or you have no rights to delete http 404 or 401 will be thrown
+    return pinterest.delete_pin(pin_id='pin_id')
+
+
+def follow():
+    # even if you already follow this user a successful message is returned
+    return pinterest.follow_user(user_id='target_user_id', username='target_username')
+
+
+def unfollow():
+    # even if you don't follow this user a successful message is returned
+    return pinterest.unfollow_user(user_id='target_user_id', username='target_username')
+
+
+def get_following():
+    # you can get following on any user, default is current user
+    # pinterest.get_following(username='some_user')
+
+    following = []
+    following_batch = pinterest.get_following()
+    while len(following) > 0:
+        following += following_batch
+        following_batch = pinterest.get_following()
+    return following
+
+
+def get_followers():
+    # you can get followers on any user, default is current user
+    # pinterest.get_user_followers(username='some_user')
+
+    followers = []
+    followers_batch = pinterest.get_user_followers()
+    while len(followers_batch) > 0:
+        followers += followers_batch
+    return followers
+
+
+def get_home_feed():
+    # This is what pinterest displays on your home page
+    # useful for auto repins
+    home_feed_pins = []
+    home_feed_batch = pinterest.home_feed()
+    while len(home_feed_batch) > 0:
+        home_feed_pins += home_feed_batch
+        home_feed_batch = pinterest.home_feed()
+
+
+def repin():
+    return pinterest.repin(board_id='board_id', pin_id='pin_id')
+
+
+def get_website_pinnable_images():
+    # Pinterest endpoint that gives all images on website
+    return pinterest.get_pinnable_images(url='https://www.tumblr.com/search/food')
+
+
+def get_board_pin_recommendations():
+    # Recommended pins for board
+    board_url = 'board_url'
+    board_id = 'board_id'
+    rec_pins = []
+    rec_batch = pinterest.board_recommendations(board_url=board_url, board_id=board_id)
+    while len(rec_batch) > 0:
+        rec_pins += rec_batch
+
+    return rec_pins
+
+
+def pin():
+    board_id = 'board_id'
+    image_url = 'image.url.com'
+    description = 'this is auto pin'
+    title = 'a bot did this'
+    return pinterest.pin(board_id=board_id, image_url=image_url, description=description, title=title)
+
+
+def search():
+    # current pinterest scopes are: pins, buyable_pins, my_pins, videos, users, boards
+    results = []
+    search_batch = pinterest.search(scope='boards', query='food')
+    while len(search_batch) > 0:
+        results += search_batch
+
+    return search_batch
+
+
+def follow_board():
+    board_url = 'board_url'
+    board_id = 'board_id'
+    return pinterest.follow_board(board_url=board_url, board_id=board_id)
+
+
+def unfollow_board():
+    board_url = 'board_url'
+    board_id = 'board_id'
+    return pinterest.unfollow_board(board_url=board_url, board_id=board_id)
+
+
+def invite():
+    board_url = 'board_url'
+    board_id = 'board_id'
+    target_user_id = 'user_id'
+    return pinterest.invite(board_id=board_id, board_url=board_url, user_id=target_user_id)
+
+
+def delete_invite():
+    board_url = 'board_url'
+    board_id = 'board_id'
+    target_user_id = 'user_id'
+    return pinterest.delete_invite(board_id=board_id, board_url=board_url, invited_user_id=target_user_id)
+
+
+def get_board_invites():
+    board_url = 'board_url'
+    board_id = 'board_id'
+    invites = []
+    invites_batch = pinterest.get_board_invites(board_url=board_url, board_id=board_id)
+    while len(invites_batch) > 0:
+        invites += invites_batch
+    return invites
+
+
+def comment_on_pin():
+    # Forbidden and not found are thrown if you don't have permissions or comment does not exist
+    pin_id = 'pin_id'
+    comment_text = 'spammy comment'
+    return pinterest.comment(pin_id=pin_id, text=comment_text)
+
+
+def delete_comment():
+    # Forbidden and not found are thrown if you don't have permissions or comment does not exist
+    pin_id = 'pin_id'
+    comment_id = 'comment_id'
+    return pinterest.delete_comment(pin_id=pin_id, comment_id=comment_id)
+
+
+def get_pin_comments():
+    return pinterest.get_comments(pin_id='pin_id')
+
+
+def load_pin_by_id():
+    return pinterest.load_pin(pin_id='pin_id')
---
+++ setup.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import setup, Command
+
+NAME = 'py3-pinterest'
+PACKAGE = 'py3pin'
+DESCRIPTION = 'Unofficial pinterest api implemented in python 3'
+URL = 'https://github.com/bryand1/python-pinterest-api'
+EMAIL = 'boriostoilov@gmail.com'
+AUTHOR = 'Borislav Stoilov'
+REQUIRES_PYTHON = '>=3.6.0'
+REQUIRED = ['requests', 'beautifulsoup4', 'requests-toolbelt']
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+about = {}
+with open(os.path.join(here, PACKAGE, '__version__.py')) as f:
+    exec(f.read(), about)
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    install_requires=REQUIRED,
+    include_package_data=True,
+    packages=[PACKAGE],
+    zip_safe=False,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
