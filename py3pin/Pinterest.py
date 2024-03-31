edbbdfad3299a1b0419793bbf3ce29753b77d2c9
Kobi Felton <kobi.c.f@gmail.com>
2024-03-31 14:36:58 -0400

    Also remove service

--- py3pin/Pinterest.py
+++ py3pin/Pinterest.py
@@ -196,9 +196,7 @@ class Pinterest:
             http_proxy.ssl_proxy = proxy
             http_proxy.add_to_capabilities(chrome_options)
 
-        driver = webdriver.Chrome(
-                service=Service(executable_path=ChromeDriverManager().install()), options=chrome_options
-        )
+        driver = webdriver.Chrome(options=chrome_options)
         driver.get("https://pinterest.com/login")
 
         try:
